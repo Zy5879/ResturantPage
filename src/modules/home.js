8a3ee40bd920c989de89b0aa0d1c509281095b19
Zy5879 <ZaireM21@gmail.com>
2022-10-13 14:33:38 -0400

    append style changes

--- src/modules/home.js
+++ src/modules/home.js
@@ -2,21 +2,27 @@
 
 function createNav() {
     const div = document.createElement('div')
-    div.innerHTML = 'SeaMonsters SeaFood Shop'
-    return div;
-}
-
-function createUl() {
+    div.classList.add('nav')
+    const p = document.createElement('p')
+    p.classList.add('color-TBA');
+    p.innerHTML = 'SeaMonsters SeaFood Shop'
     const a = document.createElement('a')
+    a.classList.add('toggle-button')
     const ul =  document.createElement('ul')
+    ul.classList.add('menu')
     const li = document.createElement('li')
+    li.classList.add('menu-options')
     const li2 = document.createElement('li')
+    li2.classList.add('menu-options')
     ul.append(li,li2)
     a.appendChild(ul);
-    return a;
+    
+    div.append(p,a)
+    return div
 }
+
 
 export {
     createNav,
-    createUl
+//     createUl
 };
