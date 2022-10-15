0f0325e5d7797a20b0479a8a09695070f8f09ba2
Zy5879 <ZaireM21@gmail.com>
2022-10-15 18:00:52 -0400

    created function that create elements inside content div

--- src/modules/home.js
+++ src/modules/home.js
@@ -5,7 +5,7 @@ function createNav() {
     div.classList.add('nav')
     const p = document.createElement('p')
     p.classList.add('color-TBA');
-    p.innerHTML = 'SeaMonsters SeaFood Shop'
+    p.innerHTML = 'SEAMONSTERS SEAFOOD SHOP'
     const a = document.createElement('a')
     a.classList.add('toggle-button')
     const ul =  document.createElement('ul')
@@ -17,12 +17,61 @@ function createNav() {
     ul.append(li,li2)
     a.appendChild(ul);
     
+    p.appendChild(a)
     div.append(p,a)
-    return div
+    return div;
+
+
 }
+
+
+function createImg() {
+   const imgDiv = document.createElement('div')
+   imgDiv.classList.add('bg-img')
+   return imgDiv;
+}
+
+function navOptions() {
+    const navmain = document.createElement('nav');
+    navmain.classList.add('main-nav')
+    
+    const ulnav = document.createElement('ul')
+    ulnav.classList.add('nav-options')
+
+    const item1 = document.createElement('li')
+    item1.classList.add('lisitem')
+    item1.innerHTML = 'Home';
+
+    const item2 = document.createElement('li')
+    item2.innerHTML = 'About';
+    item2.classList.add('lisitem')
+
+    const item3 = document.createElement('li')
+    item3.classList.add('lisitem')
+    item3.innerHTML = 'Menu';
+
+    const item4 = document.createElement('li')
+    item4.classList.add('lisitem')
+    item4.innerHTML = 'Contact';
+
+    ulnav.append(item1,item2,item3,item4);
+
+    navmain.appendChild(ulnav)
+    return navmain;
+}
+
+function catchToggle() {
+    document.addEventListener('DOMContentLoaded',(event) => {
+        document.querySelector('.toggle-button').addEventListener('click', () => {
+            document.querySelector('body').classList.toggle('toggled');
+        });
+    });
+};
 
 
 export {
     createNav,
-//     createUl
+    catchToggle,
+    createImg,
+    navOptions
 };
