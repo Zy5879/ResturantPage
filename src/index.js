010148a8a66b446a3497f4c5b135da804b850445
Zy5879 <ZaireM21@gmail.com>
2022-10-23 15:49:36 -0400

    changed background image/minor tweaks

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-// import {head, readToggle, createNav, backGround, createTitle} from './modules/load'
-// import './style.css'
-// head()
-// readToggle()
-// createNav()
-// backGround()
-// createTitle()
-// import {navHeader, mainBody, footer, clickToggle, menuNav} from './modules/load'
-// import './modules/load.js'
-// import './style.css'
-// import './modules/menu'
-// navHeader()
-// mainBody()
-// footer()
-// clickToggle()
-// menuNav()
-// import { funcMain } from './modules/load.js'
 import './modules/load.js'
 import './modules/menu.js'
 import './modules/home.js'
--- src/modules/home.js
+++ src/modules/home.js
@@ -1,7 +1,5 @@
-// const main = document.querySelector('.main-body')
-
-const main = document.getElementById('main-body')
-
+const overlay = document.querySelector('.overlay')
+const maininfo = document.querySelector('.maininfo')
 function funcNewMain() {
     const mainh1 = document.createElement('h2')
     mainh1.classList.add('about')
@@ -9,11 +7,12 @@ function funcNewMain() {
     const mainp = document.createElement('p')
     mainp.classList.add('lorem')
     mainp.textContent = 'Lorem ipsum, dolor  amet consectetur adipisicing elit. Dolores, delectus ut. Temporibus consequatur nostrum, culpa quam laudantium enim tenetur tempore aliquid animi esse praesentium aperiam aut dicta facilis quae officiis molestias sit itaque, sapiente ducimus nemo? Molestiae voluptatum at sunt.';
-    main.append(mainh1,mainp)
+    
+    overlay.append(mainh1, mainp)
 }
 
 const homeBtn = document.querySelector('.home').addEventListener('click', () => {
-    const homecontent = document.getElementById('main-body')
+    const homecontent = document.querySelector('.overlay')
     while(homecontent.firstChild) {
         homecontent.removeChild(homecontent.firstChild);
     }
